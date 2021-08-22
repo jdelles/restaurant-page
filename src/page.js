d2076aa1a9a04d1b0cf0178c031a490f8c56dc1c
jdelles <james.delles@gmail.com>
2021-08-21 21:57:03 -0400

    framing homepage header-main-footer

--- src/page.js
+++ src/page.js
@@ -1,27 +1,50 @@
 function loadHeader() {
     const header = document.createElement("header");
-    header.classList.add("header");
-  
+    
     const name = document.createElement("h1");
-    name.classList.add("restaurant-name");
     name.textContent = "Impastable Restaurante";
   
     header.appendChild(name);
 
-    // add load nav
-  
+    // add nav
+    const nav = document.createElement("nav"); 
+    const list = document.createElement("ul"); 
+    const home = document.createElement("li");
+    home.textContent = "Home"; 
+    const menu = document.createElement("li"); 
+    menu.textContent = "Menu"; 
+    const contact = document.createElement("li"); 
+    contact.textContent = "Contact"; 
+
+    list.appendChild(home);
+    list.appendChild(menu); 
+    list.appendChild(contact); 
+
+    nav.appendChild(list); 
+
+    header.appendChild(nav); 
+
     return header;
   }
 
-// function loadMain() - load separate bundles for home / menu / contact pages
+function loadMain() {
+    const main = document.createElement("main"); 
 
-// function loadFooter()
+    return main;
+}
+
+function loadFooter() {
+    const footer = document.createElement("footer"); 
+
+
+    return footer; 
+}
 
 function load() {
     const content = document.getElementById("content");
     content.appendChild(loadHeader()); 
-    // add load main
-    // add load footer
+    content.appendChild(loadMain()); 
+    content.appendChild(loadFooter()); 
   }
  
   export default load; 
